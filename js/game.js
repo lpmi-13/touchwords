b077e17175be6d73b178402853723e231d064e37
lpmi-13 <leskis@gmail.com>
2019-05-18 19:29:11 +0100

    convert to relative file paths

--- js/game.js
+++ js/game.js
@@ -312,7 +312,7 @@ function update() {}
 
 var bootState = {
   preload: function() {
-    game.load.image('preloader', '/assets/images/loading.png');
+    game.load.image('preloader', './assets/images/loading.png');
 
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
@@ -329,17 +329,17 @@ var loadState = {
   preload: function() {
     console.log('loading assets');
 
-    game.load.image('logo', 'assets/images/logo.jpg');
-    game.load.text('leveldata', 'js/levels.json');
+    game.load.image('logo', './assets/images/logo.jpg');
+    game.load.text('leveldata', './js/levels.json');
 
-    game.load.image('heart', 'assets/images/Heart.png');
-    game.load.image('button', 'assets/images/roundedColoredButton.png');
-    game.load.image('deleteButton', 'assets/images/deleteButton.png');
-    game.load.bitmapFont('digitalFont', 'assets/fonts/font.png', 'assets/fonts/font.fnt');
-    game.load.image('level1Background', 'assets/images/voodoo_cactus_island_scaled.jpg');
-    game.load.image('level2Background', 'assets/images/fishbgexp_scaled.jpg');
-    game.load.image('level3Background', 'assets/images/cloudsinthedesert_scaled.jpg');
-    game.load.image('diamond', 'assets/images/diamond.png');
+    game.load.image('heart', './assets/images/Heart.png');
+    game.load.image('button', './assets/images/roundedColoredButton.png');
+    game.load.image('deleteButton', './assets/images/deleteButton.png');
+    game.load.bitmapFont('digitalFont', './assets/fonts/font.png', './assets/fonts/font.fnt');
+    game.load.image('level1Background', './assets/images/voodoo_cactus_island_scaled.jpg');
+    game.load.image('level2Background', './assets/images/fishbgexp_scaled.jpg');
+    game.load.image('level3Background', './assets/images/cloudsinthedesert_scaled.jpg');
+    game.load.image('diamond', './assets/images/diamond.png');
     var loadingBar = game.add.sprite(0, game.world.height - 50, 'preloader');
     var loadingBarResizeX = game.world.width / 387;
     loadingBar.scale.setTo(loadingBarResizeX, 1);
